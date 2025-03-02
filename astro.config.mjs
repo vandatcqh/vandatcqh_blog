1f9b6a4a067c57eb119d32dfbf08a814574efabb
kadirlofca <kadirofca@outlook.com>
2025-03-01 22:05:41 -0500

    added tailwind, created a section component for page layout, added socials.ts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+	vite: {
+		plugins: [tailwindcss()],
+	},
 	site: 'https://example.com',
 	integrations: [mdx(), sitemap()],
 });
