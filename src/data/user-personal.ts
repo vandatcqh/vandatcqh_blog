fae2bce023e98834179a8b9457456fe3c794e072
kadirlofca <kadirofca@outlook.com>
2025-03-03 04:03:59 -0500

    added about section content

--- src/data/user-personal.ts
+++ src/data/user-personal.ts
@@ -1,1 +1,2 @@
 export const USER_NAME: string = "Name Lastname";
+export const USER_INTRODUCTION: string = "A short paragraph that shows user's personality by giving a glimpse of their hobbies, history, goals, and life. Meant as a final point of connection between the site visitor and user."
