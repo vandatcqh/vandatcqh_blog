9b838394d2841c25e11b35fc94f9e3d2db9e3dc8
kadirlofca <kadirofca@outlook.com>
2025-03-05 17:57:53 -0500

    Fixed duplicate tags

--- src/components/reactive/Filters.svelte
+++ src/components/reactive/Filters.svelte
@@ -12,13 +12,15 @@
 {/snippet}
 
 {#if $filters.length > 0}
-    <div transition:slide class="flex gap-4 p-4 pb-0 opacity-75">
+    <div transition:slide class="flex gap-4 p-4 pb-0">
         <svg class="w-8 h-8 my-auto stroke-primary-border" stroke-width="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
             <path d="M14 10L2 10 M10 14H2 M6 18H2 M18 6L2 6 M19 10V20M19 20L22 17M19 20L16 17" stroke-linecap="round" stroke-linejoin="round"></path>
         </svg>
 
-        {#each $filters as title}
-            {@render filter(title)}
-        {/each}
+        <div class="flex gap-4 opacity-60">
+            {#each $filters as title}
+                {@render filter(title)}
+            {/each}
+        </div>
     </div>
 {/if}
